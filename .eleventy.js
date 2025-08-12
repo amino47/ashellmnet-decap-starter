bf65cd13eb33e630edca4779aebb751bba9d77a6
Amina shellman <aminashellman@gmail.com>
2025-08-12 17:41:11 -0400

    add multi-page support

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("./src/css/");
+	eleventyConfig.addWatchTarget("./src/css/");
+
+	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
 	return {
 		dir: {
 			input: "src",
