0e2b39eb7dc435ef3779c799ee07856d5b291426
Amina shellman <aminashellman@gmail.com>
2025-08-12 17:25:58 -0400

    fix build errors

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = function(eleventyConfig) {
 	return {
 		dir: {
-		input: "src",
-		output: "public"
+			input: "src",
+			output: "public"
 		},
-	},
+	}
 }
--- src/index.md
+++ src/index.md
@@ -1,4 +1,5 @@
 ---
 title: Homepage
+layout: base.njk
 ---
 Welcome to my personal site! This site runs on the 11ty static site builder.
