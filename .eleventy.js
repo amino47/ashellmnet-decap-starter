766ee0aef03c1a2d7da94b7b7b29f067c7998d5f
Amina shellman <aminashellman@gmail.com>
2025-08-13 15:46:22 -0400

    add base config for diff webmentions implementation to elventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,22 @@
 const Webmentions = require("eleventy-plugin-webmentions");
+const pluginWebmentions = require("@chrisburnell/eleventy-cache-webmentions")
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/css/");
 	eleventyConfig.addWatchTarget("./src/css/");
 
-	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
 	eleventyConfig.addPlugin(Webmentions, {
 		domain: "ashellm.net",
 		token: "bLYhlV6taMuQuxTVAsIvWg",
 	});
+
+	eleventyConfig.addPlugin(pluginWebmentions, {
+		// These 3 fields are all required!
+		domain: "https://ashellm.net",
+		feed: `https://webmention.io/api/mentions.jf2?domain=example.com&per-page=9001&token=bLYhlV6taMuQuxTVAsIvWg`,
+		key: "children"
+	})
 
 	return {
 		dir: {
